4fb7b983265dd3a74341fbe3aa75fb05f588017e
George Skouroupathis <george_skouroupathis@hotmail.com>
2016-12-12 06:40:19 +0200

    Basic DB retrieving/redirecting working

--- config.js
+++ config.js
@@ -1,3 +1,8 @@
 var config = {};
 
+config.db={};
+config.db.port = 27017;
+config.db.name = 'mydb';
+config.db.collection = 'links';
+
 module.exports = config;
--- main.js
+++ main.js
@@ -1,6 +1,7 @@
 var express = require('express')
 var app = express()
 var path = require('path')
+var MongoClient = require('mongodb').MongoClient
 var config = require('./config')
 
 // App setup
@@ -12,6 +13,26 @@ app.use('./static', express.static(path.join(__dirname, 'public')))
 // Handlers
 app.get('/', function(req, res) {
 	res.render('index', {'title':'Title', 'message':'Hello'})
+})
+
+app.get('/:linkID([a-zA-Z0-9]+)', function(req, res) {
+	var dbConnectURL = 'mongodb://localhost:'+config.db.port+'/'+config.db.name;
+	MongoClient.connect(dbConnectURL, function (err, db) {
+	  if (err) throw err
+
+	  db.collection(config.db.collection).find({'_id':req.params.linkID}).toArray(function (err, result) {
+	    if (err) throw err
+
+			if (result.length != 0) {
+				var link = result[0].link;
+				res.status(301);
+				res.location(link);
+				res.end();
+			} else {
+				console.log('nop');
+			}
+	  })
+	})
 })
 
 // Start server
