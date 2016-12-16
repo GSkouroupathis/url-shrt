42893b7053fc737707a7a4465385df8239cc431a
George Skouroupathis <george_skouroupathis@hotmail.com>
2016-12-16 14:04:43 +0200

    Add automatic DB cleanup again

---
+++ cleanup.js
@@ -0,0 +1,21 @@
+config = require('./config')
+
+cleanUp = {
+
+	start: function(db, timeoutMillis) {
+		setInterval(function() {
+			var now = new Date();
+			var now_utc = new Date(now.getUTCFullYear(), now.getUTCMonth(), now.getUTCDate(),  now.getUTCHours(), now.getUTCMinutes(), now.getUTCSeconds());
+			db.collection(config.db.collection).deleteMany(
+				{
+					'expiry_date_utc': {'$lt': now_utc}
+				},
+				function(err, result) {
+					if (err) throw err
+				});
+		}, timeoutMillis);
+	}
+
+}
+
+module.exports = cleanUp;
